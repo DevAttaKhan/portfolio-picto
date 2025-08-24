7aadd735e55afeba91dbbec126c6664b8a62fc30
DevAttaKhan <attakhandev@gmail.com>
2025-08-24 18:36:53 +0500

    sitemap updated

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl:  'https://attakhan.dev', // Replace with your domain
+  generateRobotsTxt: true, // Generates robots.txt file
+  sitemapSize: 7000,
+  changefreq: 'weekly',
+  priority: 0.7,
+};
